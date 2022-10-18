fbcd1cd2268b82a1a333a75fb3ab0ea97d962010
Michael Scott <61732981+sctmic015@users.noreply.github.com>
2022-10-18 22:54:20 +0200

    Fighting

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
 #include <math.h>
 #include <time.h>
 #include <omp.h>
+#define _DEFAULT_SOURCE
+#include <dirent.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_master/stb_image.h"
@@ -17,6 +21,75 @@
 //    return img
 //    stbi_image_free(img);
 //}
+
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    /* create temp arrays */
+    int L[n1], R[n2];
+
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    /* Copy the remaining elements of L[], if there
+    are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    /* Copy the remaining elements of R[], if there
+    are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+/* l is for left index and r is right index of the
+sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
 
 /* Function to sort an array
    using insertion sort*/
@@ -42,7 +115,8 @@ void insertionSort(int arr[], int n)
 }
 
 int medianFliter(int window[], int n){
-    insertionSort(window, n);
+    mergeSort(window, 0, n-1);
+    //insertionSort(window, n);
     int median = window[(n+1)/2];
     return median;
 }
@@ -62,7 +136,7 @@ void filterImage(char file[]){
     unsigned char *img = stbi_load(file, &width, &height, &bpp, 3);
     if(img == NULL) {
         printf("Error in loading the image\n");
-        exit(1);
+        //exit(1);
     }
     unsigned bytePerPixel = 3;
     int *r = (int *)malloc(width * height * sizeof(int));
@@ -78,21 +152,21 @@ void filterImage(char file[]){
     }
 
     // Implement median filter
-    long windowSize = 9;
+    long windowSize = 3;
     long windowOffset =  (0.5)*windowSize - 0.5;
     int imageSize = (int) windowSize * windowSize;
     unsigned char *output = (unsigned char *)malloc(width*height*3*sizeof(unsigned char));
     long long update = 0;
-    int windowR[imageSize];
-    int windowG[imageSize];
-    int windowB[imageSize];
-    int count = 0;
-    int kcopy;
-    int lcopy;
-    #pragma omp parallel for num_threads(4)
+    //int count = 0;
+    #pragma omp parallel for
     for (int i = 0; i < height; i ++){
+        int windowR[imageSize];
+        int windowG[imageSize];
+        int windowB[imageSize];
         for (int j = 0; j < width; j ++){
-            count = 0;
+            int count = 0;
+            int kcopy;
+            int lcopy;
             for (int k = i - windowOffset; k <= i + windowOffset; k ++){
                 for (int l = j - windowOffset; l <= j + windowOffset; l ++){
                     kcopy = abs(k);
@@ -121,16 +195,78 @@ void filterImage(char file[]){
     free(r);
     free(g);
     free(b);
-    stbi_write_jpg("C:\\Users\\micha\\CLionProjects\\HPC\\5out.jpg", width, height, 3, output, 50);
+    stbi_write_jpg("C:\\Users\\micha\\CLionProjects\\HPC\\Images\\5out.jpg", width, height, 3, output, 50);
     stbi_image_free(img);
 }
 
 
 int main() {
-    printf("Wtf");
+
     time_t start, stop;
     start = time(NULL);
-    filterImage("C:/Users/micha/CLionProjects/HPC/highres.jpg");
+    // Attempt to try and do static array
+    // Count number of files
+    int count = 0;
+    struct dirent *dp;
+    DIR *dir;
+    if ((dir = opendir("C:/Users/micha/CLionProjects/HPC/Images")) == NULL) {
+        fprintf(stderr, "listdir: can't open %s\n", dir);
+    }
+    while ((dp = readdir(dir)) != NULL) {
+        if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, ".."))
+            continue;    /* skip self and parent */
+        printf("%s/%s\n", dir, dp->d_name);
+        count ++;
+    }
+    closedir(dir);
+
+    // Populate files Array
+    char** files = (char **)malloc(count * sizeof(char *));
+    struct dirent *dp2;
+    DIR *dir2;
+    if ((dir2 = opendir("C:/Users/micha/CLionProjects/HPC/Images")) == NULL) {
+        fprintf(stderr, "listdir: can't open %s\n", dir2);
+    }
+    int fileNum = 0;
+    while ((dp2 = readdir(dir2)) != NULL) {
+        if (!strcmp(dp2->d_name, ".") || !strcmp(dp2->d_name, ".."))
+            continue;    /* skip self and parent */
+        char file = dp2->d_name;
+        files[fileNum] = (char *)malloc(sizeof(char)* strlen(file));
+        strcpy(files[fileNum], file);
+        fileNum++;
+        //printf(file);
+    }
+    closedir(dir2);
+
+
+    for (int i = 0; i < count; i ++){
+        for (int j = 0; j < fileNum; j ++){
+            printf("%s",files[i][j]);
+        }
+    }
+    free(files);
+//    DIR *dir;
+//    struct dirent *ent;
+//    if ((dir = opendir ("C:/Users/micha/CLionProjects/HPC/Images")) != NULL) {
+//        /* print all the files and directories within directory */
+//        while ((ent = readdir (dir)) != NULL) {
+//            char* folder = "C:/Users/micha/CLionProjects/HPC/Images/";
+//            char* file = ent->d_name;
+//            char* combined = malloc(strlen(folder)+ strlen(file));
+//            strcpy(combined, folder);
+//            strcat(combined, file);
+//            printf("%s\n",combined);
+//            filterImage(combined);
+//            free(combined);
+//        }
+//        closedir (dir);
+//    } else {
+//        /* could not open directory */
+//        perror ("");
+//        return EXIT_FAILURE;
+//    }
+    //filterImage("C:/Users/micha/CLionProjects/HPC/highres.jpg");
     //filterImage("/home/michaelscott_scott77/hpc/highres.jpg");  // for cloud
     stop = time(NULL);
     printf("Run Time: %ld\n", stop - start);
