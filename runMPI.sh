ef338ffef8f404ee50442f27437ffd9c4e145112
Bradley Culligan <BradleyCulligan@gmail.com>
2022-11-04 23:48:22 +0200

    MPI sbatch script

--- runMPI.sh
+++ runMPI.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #SBATCH --partition=curie
-#SBATCH --time=36:00:00
-#SBATCH --nodes=1 --ntasks=2
-#SBATCH --job-name="serial"
-#SBATCH --mail-user=myxjar002@myuct.ac.za
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1 --ntasks=20
+#SBATCH --job-name="mpi20"
+#SBATCH --mail-user=cllbra005@myuct.ac.za
 #SBATCH --mail-type=ALL
 
 module load mpi/openmpi-4.0.1
@@ -14,11 +14,11 @@ for filterSize in "${filterSizes[@]}"
 do
     for iteration in {1..2}
     do
-        eval "./bin/serialFilter data/small10 out $filterSize >> time/serialSmall$filterSize"
-        eval "./bin/serialFilter data/medium30 out $filterSize >> time/serialMedium$filterSize"
-        eval "./bin/serialFilter data/large50 out $filterSize >> time/serialLarge$filterSize"
-        eval "./bin/serialFilter data/smallsize out $filterSize >> time/serialSmallsize$filterSize"
-        eval "./bin/serialFilter data/largesize out $filterSize >> time/serialLargesize$filterSize"
+        eval "mpirun ./bin/serialFilter data/small10 out $filterSize >> time/serialSmall$filterSize"
+        eval "mpirun ./bin/serialFilter data/medium30 out $filterSize >> time/serialMedium$filterSize"
+        #eval "mpirun ./bin/serialFilter data/large50 out $filterSize >> time/serialLarge$filterSize"
+        eval "mpirun ./bin/serialFilter data/smallsize out $filterSize >> time/serialSmallsize$filterSize"
+        #eval "mpirun ./bin/serialFilter data/largesize out $filterSize >> time/serialLargesize$filterSize"
         #eval "mpirun ./bin/mpiFilter data/small10 out $filterSize >> time/serialSmall$filterSize"
         #eval "mpirun ./bin/mpiFilter data/medium30 out $filterSize >> time/serialMedium$filterSize"
         #eval "mpirun ./bin/mpiFilter data/large50 out $filterSize >> time/serialLarge$filterSize"
