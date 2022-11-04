594846491e8643d217c651c75d682bfa78cd2352
Bradley Culligan <BradleyCulligan@gmail.com>
2022-11-04 23:11:27 +0200

    Serial run sbatch

--- runMedianFilter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH --partition=curie
-#SBATCH --time=00:01:00
-#SBATCH --nodes=2 --ntasks=32 --ntasks-per-node=16
-#SBATCH --job-name="HelloWorld"
-#SBATCH --mail-user=michelle.kuttel@uct.ac.za
-#SBATCH --mail-type=ALL
-module load mpi/openmpi-4.0.1
-mpirun /home/hpc48/helloWorld > run.out
