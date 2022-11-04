076ed3d7eac954ea87bc3b35957d953ca1df1137
Bradley Culligan <BradleyCulligan@gmail.com>
2022-11-04 22:39:21 +0200

    Local testing of parallel outputs

--- test/imageTest.py
+++ test/imageTest.py
@@ -1,9 +1,9 @@
 from os import listdir
 from os.path import isfile, join
-filesSerial = [f for f in listdir("dataOutSerial") if isfile(join("dataOutSerial", f))]
-filesOMP = [f for f in listdir("dataOutOMP") if isfile(join("dataOutOMP", f))]
-filesMPI = [f for f in listdir("dataOutMPI") if isfile(join("dataOutMPI", f))]
-filesHybrid = [f for f in listdir("dataOutHybrid") if isfile(join("dataOutHybrid", f))]
+filesSerial = [f for f in listdir("out/serial") if isfile(join("out/serial", f))]
+filesOMP = [f for f in listdir("out/omp") if isfile(join("out/omp", f))]
+filesMPI = [f for f in listdir("out/mpi") if isfile(join("out/mpi", f))]
+filesHybrid = [f for f in listdir("out/hybrid") if isfile(join("out/hybrid", f))]
 
 OMPCount = 0
 MPICount = 0
@@ -13,11 +13,11 @@ for i in range(len(filesSerial)):
     if (filesSerial[i] != filesMPI[i] or filesSerial[i] != filesOMP[i] or filesSerial[i] != filesHybrid[i]):
         print("Differing Image sets")
         exit(0)
-    if open("dataOutSerial/" + filesSerial[i], "rb").read() != open("dataOutOMP/" + filesOMP[i], "rb").read():
+    if open("out/serial/" + filesSerial[i], "rb").read() != open("out/omp/" + filesOMP[i], "rb").read():
         OMPCount += 1
-    if open("dataOutSerial/" + filesSerial[i], "rb").read() != open("dataOutMPI/" + filesMPI[i], "rb").read():
+    if open("out/serial/" + filesSerial[i], "rb").read() != open("out/mpi/" + filesMPI[i], "rb").read():
         MPICount += 1
-    if open("dataOutSerial/" + filesSerial[i], "rb").read() != open("dataOutHybrid/" + filesHybrid[i], "rb").read():
+    if open("out/serial/" + filesSerial[i], "rb").read() != open("out/hybrid/" + filesHybrid[i], "rb").read():
         HybridCount += 1
 
 print("Number of differing OMP Images: " + str(OMPCount))
