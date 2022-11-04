4a5784d2bf0ccda1acd234fb8b5c8138a8ae6a78
Jared May <jared@jaredmay.co.za>
2022-11-04 16:25:09 +0200

    Initial plot code

--- test/plot.py
+++ test/plot.py
@@ -2,4 +2,10 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 import numpy as np
 import pandas as pd
+import os
 
+PATH = "../time"
+
+files = os.listdir(PATH)
+
+print("Files in path are", files)
