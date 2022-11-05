6b2e54603b3488aaff66d2258e6d5b8e12962e47
sctmic015 <michaelscott.scott77@gmail.com>
2022-11-05 10:08:26 +0200

    OpenMP Results Added

--- runMPI.sh
+++ runMPI.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #SBATCH --partition=curie
 #SBATCH --time=12:00:00
-#SBATCH --nodes=1 --ntasks=20
-#SBATCH --job-name="mpi20"
-#SBATCH --mail-user=cllbra005@myuct.ac.za
+#SBATCH --nodes=6 --ntasks=60 --ntasks-per-node=10
+#SBATCH --job-name="Hyrbid60_6_SMT"
+#SBATCH --mail-user=sctmic015@myuct.ac.za
 #SBATCH --mail-type=ALL
-
+export OMP_NUM_THREADS=$SLURM_TASKS_PER_NODE
 module load mpi/openmpi-4.0.1
 
 filterSizes=(3 9 15)
@@ -14,11 +14,11 @@ for filterSize in "${filterSizes[@]}"
 do
     for iteration in {1..2}
     do
-        eval "mpirun ./bin/serialFilter data/small10 out $filterSize >> time/serialSmall$filterSize"
-        eval "mpirun ./bin/serialFilter data/medium30 out $filterSize >> time/serialMedium$filterSize"
-        #eval "mpirun ./bin/serialFilter data/large50 out $filterSize >> time/serialLarge$filterSize"
-        eval "mpirun ./bin/serialFilter data/smallsize out $filterSize >> time/serialSmallsize$filterSize"
-        #eval "mpirun ./bin/serialFilter data/largesize out $filterSize >> time/serialLargesize$filterSize"
+        eval "mpirun ./bin/hybridFilter data/small10 out $filterSize >> time/hyrbidSmall60_6_$filterSize"
+        eval "mpirun ./bin/hybridFilter data/medium30 out $filterSize >> time/hybridMedium60_6_$filterSize"
+        eval "mpirun ./bin/hybridFilter data/large50 out $filterSize >> time/hybridLarge60_6_$filterSize"
+        eval "mpirun ./bin/hybridFilter data/smallsize out $filterSize >> time/hybridSmallsize60_6_$filterSize"
+        eval "mpirun ./bin/hybridFilter data/largesize out $filterSize >> time/hybridLargesize60_6_$filterSize"
         #eval "mpirun ./bin/mpiFilter data/small10 out $filterSize >> time/serialSmall$filterSize"
         #eval "mpirun ./bin/mpiFilter data/medium30 out $filterSize >> time/serialMedium$filterSize"
         #eval "mpirun ./bin/mpiFilter data/large50 out $filterSize >> time/serialLarge$filterSize"
