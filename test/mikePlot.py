df2e8cf8316dfcf57bff0eb797f678cb0601dd50
sctmic015 <michaelscott.scott77@gmail.com>
2022-11-05 22:11:05 +0200

    OpenMP Graphing Stuff

--- test/mikePlot.py
+++ test/mikePlot.py
@@ -1,4 +1,6 @@
+import matplotlib
 import matplotlib.pyplot as plt
+#plt.style.use('science')
 import seaborn as sb
 import numpy as np
 import pandas as pd
@@ -9,8 +11,10 @@ from os.path import isfile, join
 
 # Create DataFrames for all Sets of data
 def readData():
-    filesSerial = [f for f in listdir(r"C:\Users\micha\CLionProjects\hpc\time\Serial") if isfile(join(r"C:\Users\micha\CLionProjects\hpc\time\Serial", f))]
-    filesOMP = [f for f in listdir(r"C:\Users\micha\CLionProjects\hpc\time\OpenMP") if isfile(join(r"C:\Users\micha\CLionProjects\hpc\time\OpenMP", f))]
+    filesSerial = [f for f in listdir(r"C:\Users\micha\CLionProjects\hpc\time\Serial") if
+                   isfile(join(r"C:\Users\micha\CLionProjects\hpc\time\Serial", f))]
+    filesOMP = [f for f in listdir(r"C:\Users\micha\CLionProjects\hpc\time\OpenMP") if
+                isfile(join(r"C:\Users\micha\CLionProjects\hpc\time\OpenMP", f))]
     # filesMPI = [f for f in listdir("out/mpi") if isfile(join("out/mpi", f))]
     # filesHybrid = [f for f in listdir("out/hybrid") if isfile(join("out/hybrid", f))]
     serialData = pd.DataFrame()
@@ -25,40 +29,46 @@ def readData():
         ompMean[i] = np.array([np.mean(ompData[i])])
     return serialData, ompData, serialMean, ompMean
 
+
 def ompSpeedUpGraphs(serialMean, ompMean):
-    dataTest = ['Small, Medium, Large, Smallsize, Largesize']
+    plt.rcParams['figure.figsize'] = [12,5]
+    plt.rcParams['figure.dpi'] = 300
+    dataTest = ['3', '9']
     # all speed-ups for window size 9
-    small9 = np.array([serialMean['serialSmall9']/ompMean['ompSmall209'],
-                      serialMean['serialSmall9']/ompMean['ompSmall409'],
-                      serialMean['serialSmall9']/ompMean['ompSmall609']])
-    medium9 = np.array([serialMean['serialMedium9']/ompMean['ompMedium209'],
-                       serialMean['serialMedium9']/ompMean['ompMedium409'],
-                       serialMean['serialMedium9']/ompMean['ompMedium609']])
-    large9 = np.array([serialMean['serialLarge9']/ompMean['ompLarge209'],
-                       serialMean['serialLarge9']/ompMean['ompLarge409'],
-                       serialMean['serialLarge9']/ompMean['ompLarge609']])
-    largeSize9 = np.array([serialMean['serialLargesize9']/ompMean['ompLargesize209'],
-                       serialMean['serialLargesize9']/ompMean['ompLargesize409'],
-                       serialMean['serialLargesize9']/ompMean['ompLargesize609']])
-    smallSize9 = np.array([serialMean['serialSmallsize9']/ompMean['ompSmallsize209'],
-                       serialMean['serialSmallsize9']/ompMean['ompSmallsize409'],
-                       serialMean['serialSmallsize9']/ompMean['ompSmallsize609']])
+    fig, ax = plt.subplots(ncols=2)
+    count = 0
+    for data in dataTest:
+        small = np.array([1, serialMean['serialSmall' + data] / ompMean['ompSmall20' + data],
+                          serialMean['serialSmall' + data] / ompMean['ompSmall40' + data],
+                          serialMean['serialSmall' + data] / ompMean['ompSmall60' + data]])
+        medium = np.array([1, serialMean['serialMedium' + data] / ompMean['ompMedium20' + data],
+                           serialMean['serialMedium' + data] / ompMean['ompMedium40' + data],
+                           serialMean['serialMedium' + data] / ompMean['ompMedium60' + data]])
+        large = np.array([1, serialMean['serialLarge' + data] / ompMean['ompLarge20' + data],
+                          serialMean['serialLarge' + data] / ompMean['ompLarge40' + data],
+                          serialMean['serialLarge' + data] / ompMean['ompLarge60' + data]])
+        largeSize = np.array([1, serialMean['serialLargesize' + data] / ompMean['ompLargesize20' + data],
+                              serialMean['serialLargesize' + data] / ompMean['ompLargesize40' + data],
+                              serialMean['serialLargesize' + data] / ompMean['ompLargesize60' + data]])
+        smallSize = np.array([1, serialMean['serialSmallsize' + data] / ompMean['ompSmallsize20' + data],
+                              serialMean['serialSmallsize' + data] / ompMean['ompSmallsize40' + data],
+                              serialMean['serialSmallsize' + data] / ompMean['ompSmallsize60' + data]])
 
+        ax[count].plot(small)
+        ax[count].plot(medium)
+        ax[count].plot(large)
+        ax[count].plot(smallSize)
+        ax[count].plot(largeSize)
+        ax[count].set_title("OpenMP Speed Up for Window Size " + data)
+        ax[count].set_ylabel("Speedup")
+        ax[count].set_xlabel("Core Count")
+        ax[count].set_xticks(np.arange(4), ["1", "20", "40", "60"])
+        ax[count].legend(["Small", "Medium", "Large", "SmallSize", "LargeSize"])
+        count += 1
+    # plt.show()
+    plt.savefig("graphs/OpenMPResults.jpg")
+    print(serialMean['serialLargesize9'] / ompMean['ompLargesize609'])
 
-    plt.plot(small9)
-    plt.plot(medium9)
-    plt.plot(large9)
-    plt.plot(smallSize9)
-    plt.plot(largeSize9)
-    plt.title("OpenMP Speed Up for Window Size 9")
-    plt.ylabel("Speedup")
-    plt.xlabel("Core Count")
-    plt.xticks(np.arange(3), ["20", "40", "60"])
-    plt.legend(["Small", "Medium", "Large", "SmallSize", "LargeSize"])
-    plt.show()
 
 serialData, ompData, serialMean, ompMean = readData()
 ompSpeedUpGraphs(serialMean, ompMean)
-
-
-
